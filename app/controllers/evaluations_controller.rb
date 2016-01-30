9de663f948897402afbe634b0378261bc0a56047
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-01-30 17:51:46 -0600

    Refactor evaluation actions to require moderator privileges

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -2,6 +2,8 @@ class EvaluationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    require_moderator
+
     @evaluation = Evaluation.where(
       'candidate_id' => params[:evaluation][:candidate_id],
       'user_id' => current_user.id
@@ -18,16 +20,22 @@ class EvaluationsController < ApplicationController
   end
 
   def index
+    require_moderator
+
     @candidates = Candidate.all
     @evaluations = Evaluation.order(:candidate_id).where(:user => current_user)
   end
 
   def new
+    require_moderator
+
     @evaluation = Evaluation.new
     @options = [1, 2, 3, 4, 5]
   end
 
   def show
+    require_moderator
+
     @evaluation = Evaluation.find(params[:id])
   end
 
@@ -41,5 +49,11 @@ class EvaluationsController < ApplicationController
         :goals,
         :remarks,
       )
+    end
+
+    def require_moderator
+      if !current_user.moderator?
+        redirect_to root
+      end
     end
 end
