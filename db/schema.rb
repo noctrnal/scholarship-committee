fd4f90e1f4abb9657ff270a2cfba31261f10b5ea
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-03-19 17:58:30 -0500

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130232057) do
+ActiveRecord::Schema.define(version: 20160131010233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "applications", force: :cascade do |t|
+    t.boolean  "citizen"
+    t.string   "relative_name"
+    t.string   "relative_address"
+    t.string   "relative_city"
+    t.string   "relative_state"
+    t.string   "relative_zip"
+    t.integer  "field_id"
+    t.string   "field_text"
+    t.integer  "degree_id"
+    t.string   "degree_text"
+    t.string   "school_name"
+    t.string   "school_address"
+    t.string   "school_city"
+    t.string   "school_state"
+    t.string   "school_zip"
+    t.boolean  "school_acceptance"
+    t.datetime "school_begin"
+    t.boolean  "school_finish_this_year"
+    t.string   "school_finish_this_year_text"
+    t.integer  "tuition_summer"
+    t.integer  "tuition_fall"
+    t.integer  "tuition_winter"
+    t.integer  "tuition_spring"
+    t.integer  "books_summer"
+    t.integer  "books_fall"
+    t.integer  "books_winter"
+    t.integer  "books_spring"
+    t.integer  "room_summer"
+    t.integer  "room_fall"
+    t.integer  "room_winter"
+    t.integer  "room_spring"
+    t.integer  "other_summer"
+    t.integer  "other_fall"
+    t.integer  "other_winter"
+    t.integer  "other_spring"
+    t.integer  "hours_summer"
+    t.integer  "hours_fall"
+    t.integer  "hours_winter"
+    t.integer  "hours_spring"
+    t.string   "request_type"
+    t.string   "comments"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "applications", ["degree_id"], name: "index_applications_on_degree_id", using: :btree
+  add_index "applications", ["field_id"], name: "index_applications_on_field_id", using: :btree
+
   create_table "candidates", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "degrees", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -36,6 +91,12 @@ ActiveRecord::Schema.define(version: 20160130232057) do
 
   add_index "evaluations", ["user_id"], name: "index_evaluations_on_user_id", using: :btree
 
+  create_table "fields", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -51,10 +112,22 @@ ActiveRecord::Schema.define(version: 20160130232057) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.boolean  "moderator",              default: false
+    t.string   "permanent_address"
+    t.string   "permanent_city"
+    t.string   "permanent_state"
+    t.string   "permanent_zip"
+    t.string   "current_address"
+    t.string   "current_city"
+    t.string   "current_state"
+    t.string   "current_zip"
+    t.integer  "permanent_phone"
+    t.integer  "current_phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "applications", "degrees"
+  add_foreign_key "applications", "fields"
   add_foreign_key "evaluations", "users"
 end
